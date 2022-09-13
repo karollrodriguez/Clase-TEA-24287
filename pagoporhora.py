3f9fe11688e0c2425d27a448d7a1270e91e52147
karollrodriguez <112212848+karollrodriguez@users.noreply.github.com>
2022-09-12 20:30:50 -0600

    Add files via upload

---
+++ pagoporhora.py
@@ -0,0 +1,10 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+horas = input("Horas de trabajo?")
+print ("Horas trabajadas" + horas)
+
+preciohora= input ("Precio por hora?")
+print ("Precio por hora" + preciohora)
+
+Calculo= int(horas) * int(preciohora)
+print (Calculo)
+
