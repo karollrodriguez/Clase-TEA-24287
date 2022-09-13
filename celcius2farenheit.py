c15ba848a9953de20f10e9e2de107bab69ce180b
karollrodriguez <112212848+karollrodriguez@users.noreply.github.com>
2022-09-12 20:55:32 -0600

    Add files via upload

---
+++ celcius2farenheit.py
@@ -0,0 +1,18 @@
+# Tendencias e Innovacion en Tecnologia Agricola (TEA)
+
+from ast import And
+
+
+celcius = input("Temperatura en Celcius?")
+
+fahrenheit = int(celcius) * 32
+
+frase= "Temperatura en Farenheit"
+
+print (int(fahrenheit))
+
+
+
+
+
+
